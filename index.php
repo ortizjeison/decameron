b84382b95d162fb96ad69eacba174c83d32f15cf
Jeison <jeisonfernelix@gmail.com>
2021-08-08 09:41:36 -0500

    prueba con entradas

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
-    echo("Hola amigos de YouTube");
+require_once 'vendor/autoload.php';
+
+$client = new \Contentful\Delivery\Client('LxFo-33yjCtikr2f6uDVEgDab7vQNjmpO2cpQb9Azhk', 'zdo3yvufu0j6', 'master');
+
+//$entry = $client->getEntry('1Wdl56SY9aUh9LEntmmUnt');
+//echo $entry->getName();
+
+foreach ($Plan as $plan) {
+    echo $plan['photos'];
+}
 
 ?>
--- test.php
+++ test.php
@@ -4,9 +4,9 @@ require_once 'vendor/autoload.php';
 
 $client = new \Contentful\Delivery\Client('LxFo-33yjCtikr2f6uDVEgDab7vQNjmpO2cpQb9Azhk', 'zdo3yvufu0j6', 'master');
 
-phpinfo();
-//$entries = $client->getEntries();
-//echo($entries);
+
+$entries = $client->getEntries();
+echo($entries);
 
 
 //$entry = $client->getEntry('5O7jJXc2OBTm93iiZYHGoP');
