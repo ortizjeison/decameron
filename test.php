58fb1cc53cb42abda47a03cc7eaabbc27dda0e83
Jeison <jeisonfernelix@gmail.com>
2021-08-08 09:31:26 -0500

    php info

--- test.php
+++ test.php
@@ -4,8 +4,9 @@ require_once 'vendor/autoload.php';
 
 $client = new \Contentful\Delivery\Client('LxFo-33yjCtikr2f6uDVEgDab7vQNjmpO2cpQb9Azhk', 'zdo3yvufu0j6', 'master');
 
-$entries = $client->getEntries();
-echo($entries);
+phpinfo();
+//$entries = $client->getEntries();
+//echo($entries);
 
 
 //$entry = $client->getEntry('5O7jJXc2OBTm93iiZYHGoP');
