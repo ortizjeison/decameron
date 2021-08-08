e7fdb10fe1e147463d79f5ff6c640fc6a8f3ad70
Jeison <jeisonfernelix@gmail.com>
2021-08-08 09:26:13 -0500

    prueba múltiples entradas corregido

--- test.php
+++ test.php
@@ -5,10 +5,10 @@ require_once 'vendor/autoload.php';
 $client = new \Contentful\Delivery\Client('LxFo-33yjCtikr2f6uDVEgDab7vQNjmpO2cpQb9Azhk', 'zdo3yvufu0j6', 'master');
 
 $entries = $client->getEntries();
-printf($entries);
+echo($entries);
 
 
-#$entry = $client->getEntry('5O7jJXc2OBTm93iiZYHGoP');
-#echo $entry->getName();
+//$entry = $client->getEntry('5O7jJXc2OBTm93iiZYHGoP');
+//echo $entry->getName();
 
 ?>
