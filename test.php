95472867c70630a80162b55b1d04021442f86d55
Jeison <jeisonfernelix@gmail.com>
2021-08-08 09:20:47 -0500

    prueba múltiples entradas

--- test.php
+++ test.php
@@ -4,8 +4,11 @@ require_once 'vendor/autoload.php';
 
 $client = new \Contentful\Delivery\Client('LxFo-33yjCtikr2f6uDVEgDab7vQNjmpO2cpQb9Azhk', 'zdo3yvufu0j6', 'master');
 
+$entries = $client->getEntries();
+printf($entries);
 
-$entry = $client->getEntry('5O7jJXc2OBTm93iiZYHGoP');
-echo $entry->getName();
+
+#$entry = $client->getEntry('5O7jJXc2OBTm93iiZYHGoP');
+#echo $entry->getName();
 
 ?>
