6ab5528404bd231d7e998fafda1eba7903f6ebda
Jeison <jeisonfernelix@gmail.com>
2021-08-08 11:01:43 -0500

    google tag

---
+++ includes/top_nav.php
@@ -0,0 +1,28 @@
+<?php
+//Toca incluir lo de la sesión que se crea con Cognito
+?>
+
+<!DOCTYPE html>
+<html>
+
+	<head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Koala T-Shirt</title>
+        <link href="https://fonts.googleapis.com/css?family=Libre+Franklin%7COpen+Sans&display=swap" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+        <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+        
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-RVSXWBM0Q3"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-RVSXWBM0Q3');
+        </script>
+    
+    </head>
--- index.php
+++ index.php
@@ -1,28 +1,31 @@
+<body>
+
+</body>
+
 <?php
+include("includes/top_nav.php");
+
 require_once 'vendor/autoload.php';
 
 $client = new \Contentful\Delivery\Client('LxFo-33yjCtikr2f6uDVEgDab7vQNjmpO2cpQb9Azhk', 'zdo3yvufu0j6', 'master');
-
-//$entry = $client->getEntry('1Wdl56SY9aUh9LEntmmUnt');
-//echo $entry->getName();
 
 //Le indicamos la entidad que vamos a consultar
 $query = new \Contentful\Delivery\Query();
 $query->setContentType('plan');
 
 //Las recibimos
-$Plan = $client->getEntries($query);
+$Planes = $client->getEntries($query);
 
 //Las recorremos todas
-foreach ($Plan as $plan) {
+foreach ($Planes as $plan) {
 
     //Extraemos la propiedad que queramos de ellas
     echo("Plan: ".$plan['name'].":");
     echo("<br>");
  
-    //Recorremos el json de las fotos y mostramos una a una
-    foreach($plan['photos'] as $p){
-        echo($p);
+    //Recorremos el array de las fotos y mostramos una a una
+    foreach($plan['photos'] as $photo){
+        echo($photo);
         echo("<br>");
     }
     echo("<br>");
