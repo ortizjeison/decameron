43cd4fb6258bc3ac43829fd0e15cf5a3c0adcc69
Jeison <jeisonfernelix@gmail.com>
2021-08-08 10:28:21 -0500

    código comentado

--- index.php
+++ index.php
@@ -6,17 +6,21 @@ $client = new \Contentful\Delivery\Client('LxFo-33yjCtikr2f6uDVEgDab7vQNjmpO2cpQ
 //$entry = $client->getEntry('1Wdl56SY9aUh9LEntmmUnt');
 //echo $entry->getName();
 
+//Le indicamos la entidad que vamos a consultar
 $query = new \Contentful\Delivery\Query();
 $query->setContentType('plan');
 
+//Las recibimos
 $Plan = $client->getEntries($query);
 
+//Las recorremos todas
 foreach ($Plan as $plan) {
 
+    //Extraemos la propiedad que queramos de ellas
     echo("Plan: ".$plan['name'].":");
     echo("<br>");
  
-    
+    //Recorremos el json de las fotos y mostramos una a una
     foreach($plan['photos'] as $p){
         echo($p);
         echo("<br>");
