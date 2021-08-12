62181be24669914500fc79323038e1ac1deeffcb
julianalondono <juli1503londono@gmail.com>
2021-08-12 17:07:58 -0500

    eeee

--- template/index.php
+++ template/index.php
@@ -88,13 +88,41 @@
                     <!-- Portfolio Item 1-->        
                         <div class="col-md-6 col-lg-4 mb-5">
                         <?php echo $room['name'] ?>
-                            <div class="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal1">
+                            <div class="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#RoomModal<?php echo $count; ?>">
                             <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
                             <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div>
                             </div>
-                            <img class="img-fluid" src="<?php echo $room['photos']['photo1'] ?>" alt="..." />
+                            <img class="img-fluid" <?php echo $room['name'] ?>src="<?php echo $room['photos']['photo1'] ?>" alt="..." />
                             </div>
                         </div>
+        <div class="portfolio-modal modal fade" id="RoomModal<?php echo $count; ?>" tabindex="-1" aria-labelledby="RoomModal<?php echo $count; ?>" aria-hidden="true">
+            <div class="modal-dialog modal-xl">
+                <div class="modal-content">
+                    <div class="modal-header border-0"><button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button></div>
+                    <div class="modal-body text-center pb-5">
+                        <div class="container">
+                            <div class="row justify-content-center">
+                                <div class="col-lg-8">
+                                    <!-- Portfolio Modal - Title-->
+                                    <h2 class="portfolio-modal-title text-secondary text-uppercase mb-0"><?php echo $room['name'] ?></h2>
+                                    <!-- Icon Divider-->
+                                    <div class="divider-custom">
+                                        <div class="divider-custom-line"></div>
+                                        <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                                        <div class="divider-custom-line"></div>
+                                    </div>
+                                    <!-- Portfolio Modal - Image-->
+                                    <img class="img-fluid rounded mb-5" src="<?php echo $room['photos']['photo1'] ?>" alt="..." />
+                                    <!-- Portfolio Modal - Text-->
+                                    <p class="mb-4"><?php echo $room['description'] ?></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+                        <?php $count++; ?>
                     <?php  } ?>    
                 </div>
             </div>
@@ -118,14 +146,42 @@
                     <!-- Portfolio Item 1-->        
                         <div class="col-md-6 col-lg-4 mb-5">
                             <?php echo $plan['name'] ?>
-                            <div class="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal2">
+                            <div class="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#PlanModal<?php echo $count1; ?>">
                             <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
                             <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div>
                             </div>
                             <img class="img-fluid" src="<?php echo $plan['photos']['photo1'] ?>" alt="..." />
                             </div>
                         </div>
-                <?php $count++; ?>
+        <div class="portfolio-modal modal fade" id="PlanModal<?php echo $count1; ?>" tabindex="-1" aria-labelledby="PlanModal<?php echo $count1; ?>" aria-hidden="true">
+            <div class="modal-dialog modal-xl">
+                <div class="modal-content">
+                    <div class="modal-header border-0"><button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button></div>
+                    <div class="modal-body text-center pb-5">
+                        <div class="container">
+                            <div class="row justify-content-center">
+                                <div class="col-lg-8">
+                                    <!-- Portfolio Modal - Title-->
+                                    <h2 class="portfolio-modal-title text-secondary text-uppercase mb-0"><?php echo $plan['name'] ?></h2>
+                                    <!-- Icon Divider-->
+                                    <div class="divider-custom">
+                                        <div class="divider-custom-line"></div>
+                                        <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                                        <div class="divider-custom-line"></div>
+                                    </div>
+                                    <!-- Portfolio Modal - Image-->
+                                    <img class="img-fluid rounded mb-5" src="<?php echo $plan['photos']['photo1'] ?>" alt="..." />
+                                    <!-- Portfolio Modal - Text-->
+                                    <p class="mb-4"><?php echo $plan['description'] ?></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+                <?php $count1++; ?>
                 <?php  } ?>   
                 </div>
             </div>
@@ -169,33 +225,7 @@
         </div>
         <!-- Portfolio Modals-->
         <!-- Portfolio Modal 1--> 
-        <div class="portfolio-modal modal fade" id="portfolioModal1" tabindex="-1" aria-labelledby="portfolioModal1" aria-hidden="true">
-            <div class="modal-dialog modal-xl">
-                <div class="modal-content">
-                    <div class="modal-header border-0"><button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button></div>
-                    <div class="modal-body text-center pb-5">
-                        <div class="container">
-                            <div class="row justify-content-center">
-                                <div class="col-lg-8">
-                                    <!-- Portfolio Modal - Title-->
-                                    <h2 class="portfolio-modal-title text-secondary text-uppercase mb-0">Nombre de la vaina</h2>
-                                    <!-- Icon Divider-->
-                                    <div class="divider-custom">
-                                        <div class="divider-custom-line"></div>
-                                        <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
-                                        <div class="divider-custom-line"></div>
-                                    </div>
-                                    <!-- Portfolio Modal - Image-->
-                                    <img class="img-fluid rounded mb-5" src="https://d1ddveogffq1qm.cloudfront.net/galeon-02.jpg" alt="..." />
-                                    <!-- Portfolio Modal - Text-->
-                                    <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        
         <!-- Portfolio Modal 2--> 
         <div class="portfolio-modal modal fade" id="portfolioModal2" tabindex="-1" aria-labelledby="portfolioModal2" aria-hidden="true">
                     <div class="modal-dialog modal-xl">
